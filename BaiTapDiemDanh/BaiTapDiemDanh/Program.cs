ee587d5104545d6e02a328e2c63d8c5c56700721
khoadang1404 <khoadang.1404@gmail.com>
2022-04-26 07:49:26 +0700

    bai 2

---
+++ BaiTapDiemDanh/BaiTapDiemDanh/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapDiemDanh
+{
+    internal class Program
+    {
+        public class Animal
+        {
+
+            //public double Weight;
+            //public double Height;
+
+            public void Eat()
+            {
+                Console.WriteLine("đang ăn. . .");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine("đang phát ra tiếng kêu. . .");
+            }
+        }
+        class Cat:Animal
+        {
+            public void Run()
+            {
+                Console.WriteLine("Mèo đang chạy. . .");
+            }
+            Animal animal = new Animal();
+
+        }
+        class Bird:Animal
+        {
+            public void Fly()
+            {
+                Console.WriteLine("Chym đang bay. . .");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Cat cat = new Cat();    
+            cat.Run();  
+            cat.Eat();
+            Bird bird = new Bird(); 
+            bird.Fly(); 
+            bird.MakeSound();
+            Console.ReadKey(); 
+        }
+    }
+}
